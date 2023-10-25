9b9a3a536795cf169088f38841431b79dcad7fa2
kgonchuk <123867028+kgonchuk@users.noreply.github.com>
2023-10-25 19:43:20 +0300

    finish

--- contacts.js
+++ contacts.js
@@ -4,19 +4,15 @@ const  nanoId=require('nanoid')
 
  const contactsPath = path.join(__dirname, "db", "contacts.json");
 
-
-// TODO: задокументувати кожну функцію
 const listContacts =  async()=> {
     const data = await fs.readFile(contactsPath)
     return JSON.parse(data)
-    // ...твій код. Повертає масив контактів.
   }
   
   const getContactById= async(contactId)=> {
     const contacts= await listContacts()
     const result = contacts.find(item=>item.id===contactId)
     return result || null
-    // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
   }
   
   const removeContact=async(contactId)=> {
@@ -28,8 +24,6 @@ const listContacts =  async()=> {
     const [results]=contact.splice(index, 1)
     await  fs.writeFile(contactsPath, JSON.stringify(contact, null, 2))
     return results
-
-    // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
   }
   
   const addContact=async({name, email, phone}) =>{
@@ -40,7 +34,6 @@ const listContacts =  async()=> {
     }
     contacts.push(newContact)
     await  fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2))
-    // ...твій код. Повертає об'єкт доданого контакту. 
     return newContact
   }
   module.exports={
